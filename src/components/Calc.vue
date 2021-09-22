581f53f696c46e840ffa24fb3ffc531f0d0eced5
aleshkaaaaa <aleksey_2909@mail.ru>
2021-09-22 16:41:50 +0300

    hometask...

--- src/components/Calc.vue
+++ src/components/Calc.vue
@@ -1,17 +1,46 @@
 <template>
   <div class="calc">
     <div class="main">
-      <input v-model="op1" type="number" />
-      <input v-model="op2" type="number" />
+      <input v-model.number ="op1" id="op1" type="number" />
+      <input v-model.number ="op2" id="op2" type="number" />
       = {{ result }}
     </div>
+    <div class="error">
+      {{error}}
+    </div>
     <div class="keyboard">
-      <button @click="plus">+</button>
-      <button @click="sub">-</button>
-      <button @click="div">/</button>
-      <button @click="multi">*</button>
-      <button @click="degree">^</button>
-      <button @click="divToFull">целочисленное деление</button>
+      <br>
+      <button @click="calculate('+')">+</button>
+      <button @click="calculate('-')">-</button>
+      <button @click="calculate('/')">/</button>
+      <button @click="calculate('*')">*</button>
+      <button @click="calculate('^')">^</button>
+      <button @click="calculate('целочисленное деление')">целочисленное деление</button>
+    </div>
+    <div>
+      <br>
+      <input type="checkbox" id="checkbox" v-model="checked"> Отобразить экранную клавиатуру
+    </div>
+    <div class="keybotdNumbers">
+      <br>
+      <button @click="typeLetter(0)">0</button>
+      <button @click="typeLetter(1)">1</button>
+      <button @click="typeLetter(2)">2</button>
+      <button @click="typeLetter(3)">3</button>
+      <button @click="typeLetter(4)">4</button>
+      <button @click="typeLetter(5)">5</button>
+      <button @click="typeLetter(6)">6</button>
+      <button @click="typeLetter(7)">7</button>
+      <button @click="typeLetter(8)">8</button>
+      <button @click="typeLetter(9)">9</button>
+      <button @click="deleteLastSym()">delete</button>
+    </div>
+    <div>
+      <br>
+      <input type="radio" id="one" value="Операнд 1" v-model="picked">Операнд 1
+      <input type="radio" id="two" value="Операнд 2" v-model="picked">Операнд 2
+      <br>
+      <span>Выбрано: {{ picked }}</span>
     </div>
   </div>
 </template>
@@ -23,10 +52,59 @@ export default {
     return {
       op1: 0,
       op2: 0,
-      result: 0
+      error: '',
+      result: 0,
+      picked: ''
     }
   },
   methods: {
+    toShow () {
+    },
+    typeLetter (element) {
+      if (document.getElementById('one').checked) {
+        let text = document.getElementById('op1').value
+        text += element
+        document.getElementById('op1').value = text
+      } else if (document.getElementById('two').checked) {
+        let text = document.getElementById('op2').value
+        text += element
+        document.getElementById('op2').value = text
+      }
+    },
+    deleteLastSym () {
+      if (document.getElementById('one').checked) {
+        let text = document.getElementById('op1').value
+        text = text.slice(0, -1)
+        document.getElementById('op1').value = text
+      } else if (document.getElementById('two').checked) {
+        let text = document.getElementById('op2').value
+        text = text.slice(0, -1)
+        document.getElementById('op2').value = text
+      }
+    },
+    calculate (operation = '+') {
+      switch (operation) {
+        case '+':
+          this.plus()
+          break
+        case '-':
+          this.sub()
+          break
+        case '/':
+          this.div()
+          break
+        case '*':
+          this.multi()
+          break
+        case '^':
+          this.degree()
+          break
+        case 'целочисленное деление':
+          this.divToFull()
+          break
+      }
+    },
+
     plus () {
       const { op1, op2 } = this
       this.result = op1 + op2
@@ -37,6 +115,10 @@ export default {
     },
     div () {
       const { op1, op2 } = this
+      if (op2 === 0) {
+        this.error = 'Делить на 0 нельзя!'
+        return
+      }
       this.result = op1 / op2
     },
     multi () {
